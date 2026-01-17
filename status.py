5f6db4b7c513f8b6b0127f06b6b1a895548f7060
Thomas Altstidl <10142068+taltstidl@users.noreply.github.com>
2026-01-17 11:41:51 +0100

    Add ability to report missing datasets in status

--- status.py
+++ status.py
@@ -1,7 +1,14 @@
+import argparse
 from pathlib import Path
 
 
 def main():
+    # Parse command line arguments
+    parser = argparse.ArgumentParser('SurvBoard status report script.')
+    parser.add_argument('--report-missing', default=False, action='store_true')
+    args = parser.parse_args()
+
+    # Create and validate paths
     results_path = Path('results')
     if not results_path.exists():
         print('Results folder not found')
@@ -10,12 +17,21 @@ def main():
     if not results_path.exists():
         print('Summary sheet not found')
         return
-    num_datasets = sum(1 for _ in open(summary_path)) - 1  # for header
+
+    # Retrieve list of datasets
+    datasets = list(l.split(',')[0] for l in open(summary_path))[1:]  # for header
+    num_datasets = len(datasets)
     print('Number of datasets: {}'.format(num_datasets))
+
+    # Check status of each model (subdirectory of results)
     for model_path in results_path.iterdir():
-        num_datasets_finished = sum(1 for _ in model_path.iterdir())
+        datasets_finished = list(l.name.split('.')[0] for l in model_path.iterdir())
+        num_datasets_finished = len(datasets_finished)
         bar = '█' * num_datasets_finished + '░' * (num_datasets - num_datasets_finished)
         print(model_path.name.ljust(28), bar)
+        if args.report_missing:
+            datasets_missing = set(datasets) - set(datasets_finished)
+            print('Missing datasets: ' + ', '.join(sorted(datasets_missing)))
 
 
 if __name__ == '__main__':
