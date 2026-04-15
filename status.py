6a1595b63f882d7d7bc51657e8edf9d95b803d27
Thomas Altstidl <10142068+taltstidl@users.noreply.github.com>
2026-04-15 11:07:59 +0200

    Add ability to query metric in status script

--- status.py
+++ status.py
@@ -1,5 +1,5 @@
 import argparse
-import re
+import json
 from pathlib import Path
 
 
@@ -7,6 +7,7 @@ def main():
     # Parse command line arguments
     parser = argparse.ArgumentParser('SurvBoard status report script.')
     parser.add_argument('--report-missing', default=False, action='store_true')
+    parser.add_argument('--metric', default='none', choices=['none', 'harrell_c', 'uno_c', 'auc'])
     args = parser.parse_args()
 
     # Create and validate paths
@@ -27,12 +28,24 @@ def main():
 
     # Check status of each model (subdirectory of results)
     for model_path in results_path.iterdir():
-        datasets_finished = set(re.sub(r'_\d{2}$', '', l.stem) for l in model_path.iterdir())
+        datasets_finished = []
+        for dataset in datasets:
+            # Option 1: all folds were run in a single experiment
+            single_path = model_path / f'{dataset}.json'
+            if single_path.exists():
+                if args.metric == 'none' or args.metric in json.load(single_path.open()):
+                    datasets_finished.append(dataset)
+            # Option 2: folds were separate experiments
+            multi_paths = [model_path / f'{dataset}_{i + 1:02d}.json' for i in range(25)]
+            if all(path.exists() for path in multi_paths):
+                if args.metric == 'none' or all(args.metric in json.load(path.open()) for path in multi_paths):
+                    datasets_finished.append(dataset)
+        # datasets_finished = set(re.sub(r'_\d{2}$', '', l.stem) for l in model_path.iterdir())
         num_datasets_finished = len(datasets_finished)
         bar = '█' * num_datasets_finished + '░' * (num_datasets - num_datasets_finished)
         print(model_path.name.ljust(28), bar)
         if args.report_missing:
-            datasets_missing = set(datasets) - datasets_finished
+            datasets_missing = set(datasets) - set(datasets_finished)
             print('Missing datasets: ' + ', '.join(sorted(datasets_missing)))
 
 
